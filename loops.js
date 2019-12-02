cb60d872e63ecc29f93f02502dadc1da7b675421
Skye Geraghty <skyetay03@googlemail.com>
2019-12-02 04:07:35 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,18 @@
+function forLoop(arr)
+{
+  for (let i = 0; i < 25; i++)
+  {
+    arr.push(i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`)
+  }
+  return arr
+}
+
+function whileLoop(num)
+{
+  while(num > 0)
+  {
+    console.log(num)
+    num--
+  }
+  return "done"
+}
