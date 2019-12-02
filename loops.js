77b956c881d2dc3742c49be6faa131be6ca5b23e
Skye Geraghty <skyetay03@googlemail.com>
2019-12-02 04:17:42 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,16 +17,18 @@ function whileLoop(num)
   return "done"
 }
 
-var i = 0;
-
-function incrementVariable() 
-{
-  i = i + 1;
-  return i;
-}
 
 function doWhileLoop(num)
 {
+  
+  var i = 0;
+
+function incrementVariable() 
+{
+   i = i + 1;
+   return i;
+  }
+
   do
   {
     console.log("I run once regardless.")
